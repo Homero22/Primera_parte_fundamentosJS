d4af7a3738329eada8d457b9336bae981a44cfce
Homero <homero6834@gmail.com>
2022-10-06 17:06:54 -0500

    tipos de datos

---
+++ js/03.js
@@ -0,0 +1,38 @@
+//tipos de datos
+
+//undefined
+//let cliente 
+//console.log(typeof cliente)
+
+//const descuento = true
+//console.log( typeof descuento)
+
+//Number
+//const number1 = 22.3
+//const number2 = 20
+//const number3 = -47
+//console.log(typeof number1,typeof number2,number3)
+//String o cadenas de texto
+
+// const alumno = "Juan"
+// let producto ='monitor'
+// const number =30
+// const number2 ="30"
+// console.log(typeof alumno)
+// console.log(typeof number, typeof number2)
+
+//BigInt
+// const numeroG = BigInt(565955656655656)
+// console.log(typeof numeroG)
+// no se puede hacer operaciones con number
+
+//Symbol es un dato único
+
+// const primerSymbol = Symbol(30)
+// const segundoSymbol = Symbol(30)
+// console.log(primerSymbol.valueOf())
+// console.log(primerSymbol === segundoSymbol)
+
+//NULL
+const descuento = null
+console.log(descuento)
