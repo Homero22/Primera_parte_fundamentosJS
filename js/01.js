60a4e63ac11195ee545df65746119b1e0a0640c2
Homero <homero6834@gmail.com>
2022-10-06 16:36:31 -0500

    variables

--- js/01.js
+++ js/01.js
@@ -1,2 +1,8 @@
-//variables
-console.log('Hola muhndo')
+//variables - var let  const
+let cliente ="Homero"
+let precioDescuento=220
+// con let se puede iniciar sin valor y reasignar
+
+console.log(cliente);
+console.log(precioDescuento)
+
