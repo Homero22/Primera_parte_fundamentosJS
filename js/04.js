fbe7f4aec5f0a253db1fa0e9b96d0b6cd4443117
Homero <homero6834@gmail.com>
2022-10-07 14:09:50 -0500

    corregido

--- js/04.js
+++ js/04.js
@@ -1,2 +1,30 @@
 
 //prueba 
+//Objetos
+
+// const nombre ="Tabblet"
+// const precio = 300
+// const disponible= true
+
+const producto ={
+    nombre : "Tablet",
+    precio : 300,
+    disponible : true
+}
+
+//acceder a las propiedades
+
+console.table(producto.disponible)
+//Destructuring
+const {nombre, precio, disponible} = producto
+console.log(nombre)
+
+//Object Literal enhacement
+const autenticado = true
+const usuario = "Homero"
+//deben llamarse igual  y se puede poner 
+const nuevoUsuario ={
+    autenticado,
+    usuario
+}
+console.table(nuevoUsuario)
