9cff01eb82a36dad651cf019bb1e359de6af040f
Homero <homero6834@gmail.com>
2022-10-08 11:11:10 -0500

    ForEach and Map

---
+++ js/11.js
@@ -0,0 +1,16 @@
+//iteradores
+const tecnologias = ['HTML','HTML', 'css','Jaavscript', 'react']
+//Foreach (una vez por cada elemento del arreglo)
+
+const arrayForEach= tecnologias.forEach(function(tech){
+    return tech
+})
+
+//map ( permite crear un nuevo arreglo)
+const arrayMap= tecnologias.map(function(tech){
+    return tech
+})
+
+console.log(arrayForEach)
+console.log(arrayMap)
+
