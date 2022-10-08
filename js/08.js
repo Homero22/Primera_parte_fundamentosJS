6d26ccfc17f0c90230558661de735664dd953a05
Homero <homero6834@gmail.com>
2022-10-08 10:39:45 -0500

    Arrays

---
+++ js/08.js
@@ -0,0 +1,12 @@
+//Arrays
+
+const tecnologias =[20, true, 'react']
+
+console.log(tecnologias)
+console.table(tecnologias)
+
+console.log(tecnologias[2])
+//propiedades
+
+console.log(tecnologias.length)
+console.log(tecnologias.toString())
