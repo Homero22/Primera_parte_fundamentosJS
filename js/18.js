2855cceed5776989a58396a8572c5fcdb858877b
Homero <homero6834@gmail.com>
2022-10-11 19:25:19 -0500

    fundamentos

---
+++ js/18.js
@@ -0,0 +1,15 @@
+// comparacion y operdor estricto ( === )
+const numer1= 20
+const numer2= '20'
+
+if(numer1 === numer2){  //revisa valor y tipo de dato
+    console.log('Si')
+}else{
+    console.log('No')
+}
+
+const autenticado = true
+
+if(autenticado){
+    console.log('Sui')
+}
